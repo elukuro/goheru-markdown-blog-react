495407feb700df0be1450cae1f163bd96c698329
elukuro <heruhartantot@gmail.com>
2021-02-26 10:18:53 +0700

    article some git command that might you dont know

--- src/article/index.js
+++ src/article/index.js
@@ -4,7 +4,8 @@ const URL =
     'how-to-switch-value',
     'unique-array',
     'how-to-shorten-if-else-statment',
-    'no-undefined-value-pass-in-your-functions-anymore'
+    'no-undefined-value-pass-in-your-functions-anymore',
+    'some-git-command-that-might-you-dont-know',
 ];
 
 export default URL.reverse()
