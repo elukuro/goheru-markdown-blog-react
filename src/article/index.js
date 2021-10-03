c90e932119102ca84892f7034b90570155e699d1
elukuro <heruhartantot@gmail.com>
2021-10-03 13:49:21 +0700

    Add archive filter feature

--- src/article/index.js
+++ src/article/index.js
@@ -1,3 +1,7 @@
-const URL = require.context('./', false, /\.md$/).keys().map((item)=>item.slice(0,-3).substring(2));
+const URL = require
+  .context("./", false, /\.md$/)
+  .keys()
+  .map((item) => item.slice(0, -3).substring(2))
+  .filter((item) => item.includes(".archive") !== true);
 
-export default URL.reverse()
+export default URL.reverse();
